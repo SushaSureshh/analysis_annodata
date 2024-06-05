c935da1468405e56c6a20ec9cf7010cd47f86d7b
Susha Suresh <sushapsuresh.mec@gmail.com>
2024-06-05 08:57:44 -0400

    Cleaner code

--- reflex_ai/main.py
+++ reflex_ai/main.py
@@ -43,7 +43,15 @@ CE_loss = nn.CrossEntropyLoss()
 #                   lr = 2e-5, # default is 5e-5 as seen in the litrature, choosing
 #                              # 2e-5
 #                   eps = 1e-8 # default is 1e-8.
-#                 )
+#  
+
+# Create the learning rate scheduler.
+# Create a schedule with a learning rate that decreases linearly from the initial
+# lr set in the optimizer to 0, after a warmup period during 
+# which it increases linearly from 0 to the initial lr set in the optimizer.
+# scheduler = get_linear_schedule_with_warmup(optimizer, 
+#                                             num_warmup_steps = 0, # Default value 
+#                                             num_training_steps = total_steps)               )
 optimizer = optim.AdamW(model.parameters(), lr=0.0001)
 EPOCH=10
 
@@ -63,49 +71,48 @@ def accuracy(output, target, topk=(1,)):
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
-def train(e):
+def train(epoch_i):
     optimizer.zero_grad()
     Avg_acc, Avg_loss = [], []
     for ind, (x,y) in enumerate(train_dataloader):
-        # print(x)
-        # print(x[0].shape, x[1].shape, x[2].shape)
-        o = model((x[0], x[1]))
-        # print(o)
-        # print("output shape", o.shape)
-        loss = CE_loss(o, y)
+        # Forward pass of the model by passing the data to the model followed by the backward pass
+        outputs = model((x[0], x[1]))
+        loss = CE_loss(outputs, y)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        top1 = accuracy(o, y)
+        top1 = accuracy(outputs, y)
         Avg_acc.append(top1)
         Avg_loss.append(loss.item())
-        print("Train Epoch %s Step %s Loss %s Top1 step acc %s Top1 Avg acc %s" %(e, ind, loss.item(), top1, np.mean(Avg_acc)))
+        print("Train Epoch %s Step %s Loss %s Top1 step acc %s Top1 Avg acc %s" %(epoch_i, ind, loss.item(), top1, np.mean(Avg_acc)))
         # print("loss at step",loss.item(), ind)
         # print("Accurace at step:", train_accuracy)
 
     return Avg_loss, Avg_acc
+
+
 #TODO Test the F1 scores for each class not the accuracy
-def test(e):
+def test(epoch_i):
     Avg_acc, Avg_loss = [], []
     for ind, (x,y) in enumerate(test_dataloader):
         with torch.no_grad():
-            o = model((x[0], x[1]))
-            loss = CE_loss(o, y)
-        top1 = accuracy(o, y)
+            outputs = model((x[0], x[1]))
+            loss = CE_loss(outputs, y)
+        top1 = accuracy(outputs, y)
         Avg_acc.append(top1)
         Avg_loss.append(loss.item())
-        print("Test Epoch %s Step %s Loss %s Top1 step acc %s Top1 Avg acc %s" %(e, ind, loss.item(), top1, np.mean(Avg_acc)))
+        print("Test Epoch %s Step %s Loss %s Top1 step acc %s Top1 Avg acc %s" %(epoch_i, ind, loss.item(), top1, np.mean(Avg_acc)))
     return Avg_loss, Avg_acc
 
 def loop():
     writer = SummaryWriter(log_dir=os.path.join('./bert_exp_without_weights_2', 'tensorboard'))
-    for e in range(EPOCH):
-        Loss, Acc = train(e)
-        writer.add_scalar("Train_Loss", np.mean(Loss), e)
-        writer.add_scalar("Train_Accuracy", np.mean(Acc), e)
-        Loss, Acc = test(e)
-        writer.add_scalar("Test_Loss", np.mean(Loss), e)
-        writer.add_scalar("Test_Accuracy", np.mean(Acc), e)
+    for epoch_i in range(EPOCH):
+        Loss, Acc = train(epoch_i)
+        writer.add_scalar("Train_Loss", np.mean(Loss), epoch_i)
+        writer.add_scalar("Train_Accuracy", np.mean(Acc), epoch_i)
+        Loss, Acc = test(epoch_i)
+        writer.add_scalar("Test_Loss", np.mean(Loss), epoch_i)
+        writer.add_scalar("Test_Accuracy", np.mean(Acc), epoch_i)
 
 
 if __name__ == "__main__":
